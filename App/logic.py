2398e4331ee41b42c0709425382c35eda696b142
21salazarf <fa.salazar@uniandes.edu.co>
2025-11-09 11:52:37 -0500

    newlogic y load data

--- App/logic.py
+++ App/logic.py
@@ -1,24 +1,118 @@
 import time
+from DataStructures.List import array_list as al
+from DataStructures.List import single_linked_list as sll
+from DataStructures.Map import map_linear_probing as mlp
+from DataStructures.Map import map_separate_chaining as msc
+from DataStructures.priority_queue import priority_queue as pq
+from datetime import datetime
+import csv
+
 
 def new_logic():
-    """
-    Crea el catalogo para almacenar las estructuras de datos
-    """
-    #TODO: Llama a las funciónes de creación de las estructuras de datos
-    pass
+    catalog = {
+        "flights": al.new_list(),
+    }
+    return catalog
 
 
 # Funciones para la carga de datos
 
 def load_data(catalog, filename):
-    """
-    Carga los datos del reto
-    """
-    # TODO: Realizar la carga de datos
-    pass
+    inicio = get_time()
 
-# Funciones de consulta sobre el catálogo
+    n_archivo = "Data/" + filename
+    archivo = csv.DictReader(open(n_archivo, encoding='utf-8'))
 
+    # ------------------------------
+    # Lectura y conversión de datos
+    # ------------------------------
+    for vuelo in archivo:
+        vuelo["flight_date"] = datetime.strptime(vuelo["flight_date"], "%Y-%m-%d")
+
+        # Si los campos de hora están vacíos, se reemplazan por "00:00"
+        hora_salida = vuelo["departure_time"] if vuelo["departure_time"] != "" else "00:00"
+        hora_llegada = vuelo["arrival_time"] if vuelo["arrival_time"] != "" else "00:00"
+        vuelo["departure_time"] = datetime.strptime(hora_salida, "%H:%M").time()
+        vuelo["arrival_time"] = datetime.strptime(hora_llegada, "%H:%M").time()
+
+        # Conversión a float
+        vuelo["air_time"] = float(vuelo["air_time"]) if vuelo["air_time"] != "" else 0.0
+        vuelo["distance"] = float(vuelo["distance"]) if vuelo["distance"] != "" else 0.0
+
+        al.add_last(catalog["flights"], vuelo)
+
+    tamanio = catalog["flights"]["size"]
+
+    # ------------------------------
+    # Ordenar por fecha y hora
+    # ------------------------------
+    vuelos_ordenados = al.new_list()
+    for i in range(0, tamanio):
+        elem = al.get_element(catalog["flights"], i)
+        al.add_last(vuelos_ordenados, elem)
+
+    def sort_crit(v1, v2):
+        """Criterio de ordenamiento: fecha + hora de salida"""
+        fecha1 = v1["flight_date"]
+        fecha2 = v2["flight_date"]
+        hora1 = v1["departure_time"]
+        hora2 = v2["departure_time"]
+        if fecha1 < fecha2:
+            return True
+        elif fecha1 == fecha2 and hora1 < hora2:
+            return True
+        return False
+
+    vuelos_ordenados = al.merge_sort(vuelos_ordenados, sort_crit)
+
+    # ------------------------------
+    # Primeros y últimos 5 vuelos
+    # ------------------------------
+    primeros = []
+    ultimos = []
+    fmt_fecha = "%Y-%m-%d"
+    fmt_hora = "%H:%M"
+
+    for i in range(0, 5):
+        vuelo = al.get_element(vuelos_ordenados, i)
+        info = {
+            "fecha": vuelo["flight_date"].strftime(fmt_fecha),
+            "hora_salida": vuelo["departure_time"].strftime(fmt_hora),
+            "hora_llegada": vuelo["arrival_time"].strftime(fmt_hora),
+            "codigo_aerolinea": vuelo["airline_iata"],
+            "nombre_aerolinea": vuelo["airline_name"],
+            "aeronave": vuelo["tail_number"],
+            "origen": vuelo["origin_airport"],
+            "destino": vuelo["destination_airport"],
+            "duracion_min": round(vuelo["air_time"], 2),
+            "distancia_millas": round(vuelo["distance"], 2)
+        }
+        primeros.append(info)
+
+    for i in range(tamanio - 5, tamanio):
+        vuelo = al.get_element(vuelos_ordenados, i)
+        info = {
+            "fecha": vuelo["flight_date"].strftime(fmt_fecha),
+            "hora_salida": vuelo["departure_time"].strftime(fmt_hora),
+            "hora_llegada": vuelo["arrival_time"].strftime(fmt_hora),
+            "codigo_aerolinea": vuelo["airline_iata"],
+            "nombre_aerolinea": vuelo["airline_name"],
+            "aeronave": vuelo["tail_number"],
+            "origen": vuelo["origin_airport"],
+            "destino": vuelo["destination_airport"],
+            "duracion_min": round(vuelo["air_time"], 2),
+            "distancia_millas": round(vuelo["distance"], 2)
+        }
+        ultimos.append(info)
+
+    # ------------------------------
+    # Tiempo total y retorno
+    # ------------------------------
+    final = get_time()
+    tiempo = delta_time(inicio, final)
+
+    retorno = catalog, tiempo, tamanio, primeros, ultimos
+    return retorno
 
 def req_1(catalog):
     """
